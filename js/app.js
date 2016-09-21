3fa87063337ea8bc65df1b9cab9d8e7265cf9b90
akakaze <m2251000@gmail.com>
2016-09-22 00:50:01 +0800

    try no backbone MVC

--- js/app.js
+++ js/app.js
@@ -3,86 +3,122 @@
 */
 define(["jquery", "underscore", "backbone", "d3"], function ($, _, Backbone, d3) {
     "use strict";
-
-    const modelGameMedia = Backbone.Model.extend({
-        defaults: {
-            video: document.createElement("video")
-        },
-        initialize: function (options) {
-            var attr = this.attributes;
-            attr.video.addEventListener("loadedmetadata", this._loadedmetadata.bind(attr));
-            attr.video.addEventListener("play", this._play.bind(attr));
-            attr.video.addEventListener("pause", this._pause.bind(attr));
-        },
-        setSource: function (source) {
-            this.attributes.source = source;
-            ajax("GET", source, "arraybuffer", this._ajaxProgress)
-                .then(this._ajaxResolve.bind(this.attributes), this._ajaxReject);
-        },
-        videoToggle: function () {
-            this.attributes.video.paused ?
-                this.attributes.video.play() : this.attributes.video.pause();
-        },
-        _loadedmetadata: function () {
-            var p = this.video.videoHeight / this.video.videoWidth;
-            if (p <= 0.5625) {
-                this.height = Math.floor(1920 * p);
-                this.width = 1920;
-            }
-            else {
-                this.height = 1080;
-                this.width = Math.floor(1080 / p);
-            }
-            this.left = (1920 - this.width) / 2;
-            this.top = (1080 - this.height) / 2;
-        },
-        _play: function () {
-            this.interval = setInterval(this.view.media2canvas, 0, this);
-        },
-        _pause: function () {
-            clearInterval(this.interval);
-        },
-        _ajaxProgress: function (event) {
-            var prog = Math.floor(event.loaded * 100 / event.total);
-            console.log("progress : " + prog + "%");
-        },
-        _ajaxResolve: function (data) {
-            var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-            audioCtx.decodeAudioData(data, function (buffer) {
-                for (var channel = 0; channel < buffer.numberOfChannels; channel ++) {
-                    console.dir(buffer.getChannelData(channel));
+    const modelGameMedia_test = function (view) {
+        var $t = this,
+            $v = document.createElement("video"),
+            $i;
+        Object.defineProperties(this, {
+            view: {
+                value: view
+            },
+            video: {
+                value: $v
+            },
+            source: {
+                set: function (source) {
+                    ajax("GET", source, "arraybuffer", $t._ajaxProgress)
+                        .then($t._ajaxResolve, $t._ajaxReject);
                 }
-            }, function (e) {
-                console.error("Error with decoding audio data" + e.err);
-            });
-            var blob = new Blob([data], {
-                type: "video/mpeg4"
-            });
-            this.video.setAttribute("src", URL.createObjectURL(blob));
-            this.view.media2canvas(this);
-            this.video.pause();
-        },
-        _ajaxReject: function (data) {
-            console.error(JSON.parse(data));
-        }
-    });
+            },
+            height: {
+                value: 0,
+                writable: true
+            },
+            width: {
+                value: 0,
+                writable: true
+            },
+            top: {
+                value: 0,
+                writable: true
+            },
+            left: {
+                value: 0,
+                writable: true
+            },
+            videoToggle: {
+                value: function () {
+                    $t.video.paused ?
+                        $t.video.play() : $t.video.pause();
+                }
+            },
+            _loadedmetadata: {
+                value: function () {
+                    var p = $v.videoHeight / $v.videoWidth;
+                    if (p <= 0.5625) {
+                        $t.height = Math.floor(1920 * p);
+                        $t.width = 1920;
+                    }
+                    else {
+                        $t.height = 1080;
+                        $t.width = Math.floor(1080 / p);
+                    }
+                    $t.left = (1920 - $t.width) / 2;
+                    $t.top = (1080 - $t.height) / 2;
+                }
+            },
+            _play: {
+                value: function () {
+                    $i = setInterval($t.view.media2canvas, 0, $t);
+                }
+            },
+            _pause: {
+                value: function () {
+                    clearInterval($i);
+                }
+            },
+            _ajaxProgress: {
+                value: function (event) {
+                    var prog = Math.floor(event.loaded * 100 / event.total);
+                    console.log("progress : " + prog + "%");
+                }
+            },
+            _ajaxResolve: {
+                value: function (data) {
+                    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+                    audioCtx.decodeAudioData(data, function (buffer) {
+                        for (var channel = 0; channel < buffer.numberOfChannels; channel ++) {
+                            console.dir(buffer.getChannelData(channel));
+                        }
+                    }, function (e) {
+                        console.error("Error with decoding audio data" + e.err);
+                    });
+                    var blob = new Blob([data], {
+                        type: "video/mpeg4"
+                    });
+                    $t.video.src = URL.createObjectURL(blob);
+                    $t.view.media2canvas($t);
+                    $t.video.pause();
+                }
+            },
+            _ajaxReject: {
+                value: function (data) {
+                    console.error(JSON.parse(data));
+                }
+            }
+        });
+        $v.addEventListener("loadedmetadata", $t._loadedmetadata);
+        $v.addEventListener("play", $t._play);
+        $v.addEventListener("loadedmetadata", $t._pause);
+    };
     const viewApp = Backbone.View.extend({
         el: "#game_app",
         initialize: function () {
             this.templateGameMedia = this.el.querySelector("#game_media");
             this.templateGameMain = this.el.querySelector("#game_main");
             this.templateGameControls = this.el.querySelector("#game_control");
-            this.modelGameMedia = new modelGameMedia({view: this});
-            this.modelGameMedia.setSource("media/video/xyy_rg_test.mp4");
+
+            this.modelGameMedia = new modelGameMedia_test(this);
+            this.modelGameMedia.source = "resource/video/xyy_rg_test.mp4";
             this.media2canvas = this.media2canvas.bind(this);
         },
-        media2canvas: function (opt) {
+        media2canvas: function (mod) {
             this.templateGameMedia.getContext("2d").drawImage(
-                opt.video,
-                opt.left,
-                opt.top,
-                opt.width,
-                opt.height
+                mod.video,
+                mod.left,
+                mod.top,
+                mod.width,
+                mod.height
             );
         },
         events: {
